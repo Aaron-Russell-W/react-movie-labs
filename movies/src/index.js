07f90423a150136ee7a4d460af87ebb57fc39230
aaron <aaronruss1@hotmail.com>
2023-11-22 14:37:46 +0000

    Change Site header

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import SiteHeader from './components/siteHeader'
+
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
@@ -8,22 +10,15 @@ import MovieReviewPage from "./pages/movieReviewPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-      </Routes>
-    </BrowserRouter>
+    <SiteHeader />
+    <Routes>
+      <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+      <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={ <Navigate to="/" /> } />
+    </Routes>
+  </BrowserRouter>
   );
 };
 
