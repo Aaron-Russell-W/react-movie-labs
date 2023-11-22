ae93f7990fcc0768b2404b494ba5259f1c970943
aaron <aaronruss1@hotmail.com>
2023-11-22 14:51:22 +0000

    Upcoming page added

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -34,9 +34,17 @@ export const getMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
     )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
+    .then((res) => res.json())
+    .then((json) => {
+       console.log(json.results);
         return json.results;
-      });
+    });
   };
+
+  export const getUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  }
--- movies/src/index.js
+++ movies/src/index.js
@@ -1,24 +1,27 @@
 import React from "react";
-import SiteHeader from './components/siteHeader'
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from "./components/siteHeader";
+import UpcomingPage from "./pages/upcomingMoviesPage";
+
+
 const App = () => {
   return (
     <BrowserRouter>
-    <SiteHeader />
-    <Routes>
-      <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-      <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-      <Route path="/movies/:id" element={<MoviePage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={ <Navigate to="/" /> } />
-    </Routes>
-  </BrowserRouter>
+      <SiteHeader />
+      <Routes>
+        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
